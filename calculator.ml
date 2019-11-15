964597229ba323d16dd103e7a2ae53aa3f5bab61
oojahooo <oojahooo@gmail.com>
2019-11-15 20:47:52 +0900

    feedback

--- calculator.ml
+++ calculator.ml
@@ -1,40 +1,41 @@
-type exp = X
-		 | INT of int
-		 | ADD of exp * exp
-		 | SUB of exp * exp
-		 | MUL of exp * exp
-		 | DIV of exp * exp
-		 | SIGMA of exp * exp * exp
+type exp =
+  X
+| INT of int
+| ADD of exp * exp
+| SUB of exp * exp
+| MUL of exp * exp
+| DIV of exp * exp
+| SIGMA of exp * exp * exp
 
-let rec calculator : exp -> int
-= fun exp -> 
-  match exp with
-  | X -> raise (Failure "X")
+let rec apply : exp -> int -> int =
+fun e x ->
+  match e with
+    X -> x
   | INT n -> n
-  | ADD (e1,e2) -> binop e1 e2 (+)
-  | SUB (e1,e2) -> binop e1 e2 (-)
-  | MUL (e1,e2) -> binop e1 e2 ( * )
-  | DIV (e1,e2) -> binop e1 e2 (/)
-  | SIGMA (e1,e2,e3) -> sigma (calculator e1) (calculator e2) e3
-
-and binop e1 e2 op =
-  op (calculator e1) (calculator e2)
-
-and nbinop n e1 e2 op =
-  op (calcul_x e1 n) (calcul_x e2 n)
-
-and calcul_x exp n =
+  | ADD (e1, e2) -> apply e1 x + apply e2 x
+  | SUB (e1, e2) -> apply e1 x - apply e2 x
+  | MUL (e1, e2) -> apply e1 x * apply e2 x
+  | DIV (e1, e2) -> apply e1 x / apply e2 x
+  | SIGMA (e1, e2, e3) ->
+      let i = apply e1 x in
+      let j = apply e2 x in
+      if i > j then raise (Failure "Invalid Input")
+      else if i = j then apply e3 i
+      else apply e3 i + apply (SIGMA (ADD (e1, INT 1), e2, e3)) x
+  
+let rec calculator : exp -> int =
+fun exp ->
   match exp with
-  | X -> n
-  | INT num -> num
-  | ADD (e1,e2) -> nbinop n e1 e2 (+)
-  | SUB (e1,e2) -> nbinop n e1 e2 (-)
-  | MUL (e1,e2) -> nbinop n e1 e2 ( * )
-  | DIV (e1,e2) -> nbinop n e1 e2 (/)
-  | SIGMA (e1,e2,e3) -> sigma (calculator e1) (calculator e2) e3
-
-and sigma : int -> int -> exp -> int
-= fun n1 n2 exp ->
-  let n = calcul_x exp n2 in
-  if n1 = n2 then n
-  else (sigma n1 (n2-1) exp) + n;;
+    X -> raise (Failure "Invalid Input")
+  | INT n -> n
+  | ADD (e1, e2) -> calculator e1 + calculator e2
+  | SUB (e1, e2) -> calculator e1 - calculator e2
+  | MUL (e1, e2) -> calculator e1 * calculator e2
+  | DIV (e1, e2) -> calculator e1 / calculator e2
+  | SIGMA (e1, e2, e3) ->
+      let i = calculator e1 in
+      let j = calculator e2 in
+      if i > j then raise (Failure "Invalid Input")
+      else if i = j then apply e3 i
+      else apply e3 i + calculator (SIGMA (ADD (e1, INT 1), e2, e3))
+;;
--- diff.ml
+++ diff.ml
@@ -1,23 +1,23 @@
 type aexp =
-  | Const of int
-  | Var of string
-  | Power of string * int
-  | Times of aexp list
-  | Sum of aexp list
+  Const of int
+| Var of string
+| Power of string * int
+| Times of aexp list
+| Sum of aexp list
 
-let rec diff : aexp * string -> aexp
-= fun (exp, x) -> 
+let rec diff : aexp * string -> aexp =
+fun (exp, x) ->
   match exp with
-  | Const _ -> Const 0
-  | Var v -> if v = x then Const 1 else Const 0
-  | Power (v, n) -> if n = 0 then Const 0 else Times ((Const n)::[Power (v, (n-1))])
-  | Times [] -> Const 0
-  | Sum [] -> Const 0
-  | Times (hd::tl) -> Sum ((Times (diff (hd, x)::tl))::[Times (hd::[diff (Times tl, x)])])
-  | Sum (hd::tl) -> Sum (listdiff (hd::tl, x))
-
-and listdiff : aexp list * string -> aexp list
-= fun (explst, x) ->
-  match explst with
-  | [] -> []
-  | hd::tl -> (diff (hd, x))::listdiff (tl, x);;
+    Const n -> Const 0
+  | Var y -> if x = y then Const 1 else Const 0
+  | Power (y, n) ->
+      if n < 0 then raise (Failure "Invalid Input")
+      else if n = 0 || x <> y then Const 0
+      else Times [Const n; Power (y, n - 1)]
+  | Times [hd] -> diff (hd, x)
+  | Times (hd :: tl) ->
+      Sum [Times (diff (hd, x) :: tl); Times [hd; diff (Times tl, x)]]
+  | Sum [hd] -> diff (hd, x)
+  | Sum (hd :: tl) -> Sum [diff (hd, x); diff (Sum tl, x)]
+  | _ -> raise (Failure "Invalid Input")
+;;
