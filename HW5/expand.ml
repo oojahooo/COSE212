d553f87eb9c3e6ace52c0d93e530747834a034e3
oojahooo <oojahooo@gmail.com>
2019-11-28 12:49:02 +0900

    add HW5 files

---
+++ HW5/expand.ml
@@ -0,0 +1,20 @@
+type program = exp
+and exp = 
+  | CONST of int
+  | VAR of var
+  | ADD of exp * exp
+  | SUB of exp * exp
+  | MUL of exp * exp
+  | DIV of exp * exp
+  | ISZERO of exp
+  | READ
+  | IF of exp * exp * exp
+  | LET of var * exp * exp
+  | LETREC of var * var * exp * exp
+  | PROC of var * exp
+  | CALL of exp * exp
+and var = string
+
+let expand : exp -> exp 
+=fun e -> e (* TODO *)
+;;
