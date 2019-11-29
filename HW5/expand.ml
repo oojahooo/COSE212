e8eb2f612ffe202cad484734a5957cb93d8577f0
oojahooo <oojahooo@gmail.com>
2019-11-29 14:10:27 +0900

    complete HW5 codes

--- HW5/expand.ml
+++ HW5/expand.ml
@@ -15,6 +15,58 @@ and exp =
   | CALL of exp * exp
 and var = string
 
-let expand : exp -> exp 
-=fun e -> e (* TODO *)
+let rec used : var -> exp -> bool
+= fun x e ->
+  match e with
+  | CONST n -> false
+  | VAR v -> v = x
+  | ADD (e1, e2) -> (used x e1) || (used x e2)
+  | SUB (e1, e2) -> (used x e1) || (used x e2)
+  | MUL (e1, e2) -> (used x e1) || (used x e2)
+  | DIV (e1, e2) -> (used x e1) || (used x e2)
+  | ISZERO e' -> used x e'
+  | READ -> false
+  | IF (e1, e2, e3) -> (used x e1) || (used x e2) || (used x e3)
+  | LET (v, e1, e2) -> if v = x then used x e1 else (used x e1) || (used x e2)
+  | LETREC (v1, v2, e1, e2) ->
+    if v1 = x then false else
+    if v2 = x then used x e2 else
+    (used x e1) || (used x e2) 
+  | PROC (v, e') -> if v = x then false else used x e'
+  | CALL (e1, e2) -> (used x e1) || (used x e2)
+
+let rec substitute : var -> exp -> exp -> exp
+= fun x e1 e2 ->
+  match e2 with
+  | CONST n -> e2
+  | VAR v -> if v = x then e1 else e2
+  | ADD (e1', e2') -> ADD (substitute x e1 e1', substitute x e1 e2')
+  | SUB (e1', e2') -> SUB (substitute x e1 e1', substitute x e1 e2')
+  | MUL (e1', e2') -> MUL (substitute x e1 e1', substitute x e1 e2')
+  | DIV (e1', e2') -> DIV (substitute x e1 e1', substitute x e1 e2')
+  | ISZERO e' -> ISZERO (substitute x e1 e')
+  | READ -> e2
+  | IF (e1', e2', e3') -> IF (substitute x e1 e1', substitute x e1 e2', substitute x e1 e3')
+  | LET (v, e1', e2') -> if v = x then LET (v, substitute x e1 e1', e2') else LET (v, substitute x e1 e1', substitute x e1 e2')
+  | LETREC (v1, v2, e1', e2') ->
+    if v1 = x then e2 else
+    if v2 = x then LETREC (v1, v2, e1', substitute x e1 e2') else
+    LETREC (v1, v2, substitute x e1 e1', substitute x e1 e2')
+  | PROC (v, e') -> if v = x then e2 else PROC (v, substitute x e1 e')
+  | CALL (e1', e2') -> CALL (substitute x e1 e1', substitute x e1 e2')
+
+let rec expand : exp -> exp 
+= fun e ->
+  match e with
+  | LET (x, e1, e2) -> if used x e2 then substitute x e1 (expand e2) else LET (x, e1, expand e2)
+  | ADD (e1, e2) -> ADD (expand e1, expand e2)
+  | SUB (e1, e2) -> SUB (expand e1, expand e2)
+  | MUL (e1, e2) -> MUL (expand e1, expand e2)
+  | DIV (e1, e2) -> DIV (expand e1, expand e2)
+  | ISZERO e' -> ISZERO (expand e')
+  | IF (e1, e2, e3) -> IF (expand e1, expand e2, expand e3)
+  | LETREC (f, x, e1, e2) -> LETREC (f, x, expand e1, expand e2)
+  | PROC (x, e') -> PROC (x, expand e')
+  | CALL (e1, e2) -> CALL (expand e1, expand e2)
+  | _ -> e
 ;;
--- HW5/type.ml
+++ HW5/type.ml
@@ -48,7 +48,7 @@ module Subst = struct
 
   (* walk through the type, replacing each type variable by its binding in the substitution *)
   let rec apply : typ -> t -> typ
-  =fun typ subst ->
+  = fun typ subst ->
     match typ with
     | TyInt -> TyInt
     | TyBool -> TyBool 
@@ -62,7 +62,7 @@ module Subst = struct
     (tv,ty) :: (List.map (fun (x,t) -> (x, apply t [(tv,ty)])) subst)
 
   let print : t -> unit
-  =fun subst -> 
+  = fun subst -> 
       List.iter (fun (x,ty) -> print_endline (x ^ " |-> " ^ string_of_type ty)) subst
 end
 
@@ -72,13 +72,68 @@ let tyvar_num = ref 0
 let fresh_tyvar () = (tyvar_num := !tyvar_num + 1; (TyVar ("t" ^ string_of_int !tyvar_num)))
 
 let rec gen_equations : TEnv.t -> exp -> typ -> typ_eqn 
-=fun tenv e ty -> [] (* TODO *)
+= fun tenv e ty ->
+  match e with
+  | CONST n -> [(ty, TyInt)]
+  | VAR x -> [(ty, TEnv.find tenv x)]
+  | ADD (e1, e2) -> [(ty, TyInt)]@(gen_equations tenv e1 TyInt)@(gen_equations tenv e2 TyInt)
+  | SUB (e1, e2) -> [(ty, TyInt)]@(gen_equations tenv e1 TyInt)@(gen_equations tenv e2 TyInt)
+  | MUL (e1, e2) -> [(ty, TyInt)]@(gen_equations tenv e1 TyInt)@(gen_equations tenv e2 TyInt)
+  | DIV (e1, e2) -> [(ty, TyInt)]@(gen_equations tenv e1 TyInt)@(gen_equations tenv e2 TyInt)
+  | ISZERO e' -> [(ty, TyBool)]@(gen_equations tenv e' TyInt)
+  | READ -> [(ty, TyInt)]
+  | IF (e1, e2, e3) -> (gen_equations tenv e1 TyBool)@(gen_equations tenv e2 ty)@(gen_equations tenv e3 ty)
+  | LET (x, e1, e2) -> let a = fresh_tyvar () in (gen_equations tenv e1 a)@(gen_equations (TEnv.extend (x, a) tenv) e2 ty)
+  | LETREC (f, x, e1, e2) ->
+    let a1, a2 = (fresh_tyvar (), fresh_tyvar ()) in
+    let tenv' = TEnv.extend (f, TyFun (a1, a2)) tenv in
+    (gen_equations (TEnv.extend (x, a1) tenv') e1 a2)@(gen_equations tenv' e2 ty)
+  | PROC (x, e') ->
+    let a1 = fresh_tyvar () in
+    let a2 = fresh_tyvar () in
+    [(ty, TyFun (a1, a2))]@(gen_equations (TEnv.extend (x, a1) tenv) e' a2)
+  | CALL (e1, e2) -> let a = fresh_tyvar () in (gen_equations tenv e1 (TyFun (a, ty)))@(gen_equations tenv e2 a)
+
+let rec occurs : tyvar -> typ -> bool
+= fun a t ->
+  match t with
+  | TyInt -> false
+  | TyBool -> false
+  | TyVar a' -> a = a'
+  | TyFun (t1, t2) -> (occurs a t1) || (occurs a t2)
+
+let rec unify : typ -> typ -> Subst.t -> Subst.t
+= fun t1 t2 subst ->
+  match t1, t2 with
+  | TyInt, TyInt -> subst
+  | TyBool, TyBool -> subst
+  | TyVar a, t ->
+    (match t with
+    | TyVar a' -> if a = a' then subst else Subst.extend a (TyVar a') subst
+    | _ -> if occurs a t then raise TypeError else Subst.extend a t subst)
+  | t, TyVar a -> unify (TyVar a) t subst
+  | TyFun (a1, a2), TyFun(a1', a2') ->
+    let subst' = unify a1 a1' subst in
+    let subst'' = unify (Subst.apply a2 subst') (Subst.apply a2' subst') subst' in
+    subst''
+  | _, _ -> raise TypeError
+
+let rec unifyall : typ_eqn -> Subst.t -> Subst.t
+= fun eqns subst ->
+  match eqns with
+  | [] -> subst
+  | (t1, t2)::u ->
+    let subst' = unify (Subst.apply t1 subst) (Subst.apply t2 subst) subst in
+    (* print_endline ("========");
+    Subst.print subst'; *)
+    unifyall u subst'
 
 let solve : typ_eqn -> Subst.t
-=fun eqns -> Subst.empty (* TODO *)
+= fun eqns ->
+  unifyall eqns Subst.empty
 
 let typeof : exp -> typ 
-=fun exp ->
+= fun exp ->
   let new_tv = fresh_tyvar () in
   let eqns = gen_equations TEnv.empty exp new_tv in
   let _ = print_endline "= Equations = ";
